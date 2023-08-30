12c0355db382c148940b96dd865eb3fbaa5a3076
lohar prathmesh <loharprathmesh123@gmail.com>
2023-08-30 17:12:53 +0530

    vercel

--- web_university/settings.py
+++ web_university/settings.py
@@ -82,10 +82,21 @@ WSGI_APPLICATION = 'web_university.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'webuniversity',
+        'USER': 'prathmesh-lohar',
+        'PASSWORD': 'rwoX6cFg5Rbd',
+        'HOST': 'ep-spring-voice-59930502.ap-southeast-1.aws.neon.tech',
+        'PORT': '5432',
     }
 }
 
--- web_university/wsgi.py
+++ web_university/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+app = application
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web_university.settings')
 
 application = get_wsgi_application()
