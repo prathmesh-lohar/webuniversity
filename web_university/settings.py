499c7b26c302e677d255dfaf6f3b05b187e610e1
lohar prathmesh <loharprathmesh123@gmail.com>
2023-08-30 17:52:44 +0530

    pre

--- web_university/settings.py
+++ web_university/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'web_university.wsgi.application'
 
 
 # Database
-https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+#https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
